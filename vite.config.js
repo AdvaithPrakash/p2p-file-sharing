9d4192cfa4a480ca86f6c57f0a742970dd5780b2
Advaith Prakash <advaith@Advaiths-MacBook-Air-1050.local>
2025-09-15 04:39:07 -0700

    Fix Vite build: move build tools to dependencies, create root config files

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  root: './client',
+  build: {
+    outDir: './client/dist',
+    emptyOutDir: true
+  },
+  server: {
+    port: 3000,
+    proxy: {
+      '/socket.io': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        ws: true
+      }
+    }
+  }
+})
